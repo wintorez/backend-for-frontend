f07e5afb432db705f8e06e7c7b9799140be9faba
Reza Sadr <wintorez@gmail.com>
2024-02-06 16:45:56 -0500

    Add logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,19 @@
 import express, { Express, Request, Response } from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
 const app: Express = express()
 const port = process.env.PORT || 8443
 
+app.use(helmet())
+app.use(morgan('combined'))
+
 app.get('/api/message', (req: Request, res: Response) => {
-  res.send('Hello from BFF!')
+  res.send('Hello World!')
 })
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`)
 })
