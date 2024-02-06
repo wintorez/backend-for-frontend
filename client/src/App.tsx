4052d181dc58882ff19ae08c80486c07da6b57bf
Reza Sadr <wintorez@gmail.com>
2024-02-06 10:23:24 -0500

    Update settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const messageQuery = useSWR('/api/message', () => axios.get('/api/message').then(r => r.data))
+  const messageQuery = useSWR('/api/message', () => axios.get('/api/message').then((r) => r.data), {
+    shouldRetryOnError: true,
+  })
 
   return (
     <>
@@ -16,12 +18,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        {messageQuery.data}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">{messageQuery.data}</div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
